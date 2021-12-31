0e4b89f378ab5542e181880e77f72117a2d83b5b
jpg1129 <james.gomatos@gmail.com>
2021-12-31 18:38:51 -0500

    basic app done

--- app/Components/Popular.js
+++ app/Components/Popular.js
@@ -9,6 +9,7 @@ import {
   FaStar,
 } from 'react-icons/fa';
 import Card from './Card';
+import Loading from './Loading';
 
 function LanguagesNav({ selected, onUpdateLanguage }) {
   const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
@@ -131,7 +132,7 @@ export default class Popular extends React.Component {
           selected={selectedLanguage}
           onUpdateLanguage={this.updateLanguage}
         />
-        {this.isLoading() && <p>LOADING...</p>}
+        {this.isLoading() && <Loading text="Fetching Repos" />}
         {error && <p className="center-text error">{error}</p>}
         {repos[selectedLanguage] && (
           <ReposGrid repos={repos[selectedLanguage]} />
